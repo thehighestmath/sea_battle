d24999bcfbc5cfaf869fa43d5173dc0e8983c1da
thehighestmath <kirill.lfybk.rh@gmail.com>
2021-03-28 01:48:10 +0300

    implement requirements | add requement for pip

--- src/GameWindow.py
+++ src/GameWindow.py
@@ -5,15 +5,16 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QGraphicsBlurEffect
 
+from states import States
 from gamewindow_ui import Ui_GameWindow
 
 
-class GameWindow(QtWidgets.QMainWindow):
+class GameWindow(QtWidgets.QWidget):
     def __init__(self, parent=None):
         super(GameWindow, self).__init__(parent)
         self.ui = Ui_GameWindow()
         self.ui.setupUi(self)
-        self.setStyleSheet('''QMainWindow{
+        self.setStyleSheet('''#centralWidget{
         border-image: url(ship.jpg) 0 0 0 0 stretch stretch;
         }
         QLabel{
@@ -28,12 +29,17 @@ class GameWindow(QtWidgets.QMainWindow):
         self.ui.actionPlayer_2.triggered.connect(self.player_2)
 
     def mousePressEvent(self, event):
-        logging.debug("Main Mouse Press")
+        # вдруг пригодится
+        logging.debug("Game window mouse press")
         super(GameWindow, self).mousePressEvent(event)
 
     def set_widgets_on_off(self, is_first: bool = True):
         self.ui.wigdet_player_1.setEnabled(is_first)
         self.ui.wigdet_player_2.setEnabled(not is_first)
+
+    def hide_ships(self):
+        self.ui.ships_list_1.hide()
+        self.ui.ships_list_2.hide()
 
     def prepare_player_1(self):
         logger = logging.getLogger(__name__)
@@ -59,8 +65,7 @@ class GameWindow(QtWidgets.QMainWindow):
         self.ui.label_player_1.setText('[Player 1]')
         self.ui.label_player_2.setText('Player 2')
 
-        self.ui.ships_list_1.hide()
-        self.ui.ships_list_2.hide()
+        self.hide_ships()
 
         self.set_widgets_on_off(is_first=True)
         self.ui.wigdet_player_1.setGraphicsEffect(None)
@@ -73,12 +78,23 @@ class GameWindow(QtWidgets.QMainWindow):
         self.ui.label_player_1.setText('Player 1')
         self.ui.label_player_2.setText('[Player 2]')
 
-        self.ui.ships_list_1.hide()
-        self.ui.ships_list_2.hide()
+        self.hide_ships()
 
         self.set_widgets_on_off(is_first=False)
         self.ui.wigdet_player_1.setGraphicsEffect(QGraphicsBlurEffect())
         self.ui.wigdet_player_2.setGraphicsEffect(None)
+
+    def state_machine(self, type_):
+        if type_ == States.PREPARE_PLAYER_1:
+            self.prepare_player_1()
+        elif type_ == States.PREPARE_PLAYER_2:
+            self.prepare_player_2()
+        elif type_ == States.MOVE_PLAYER_1:
+            self.player_1()
+        elif type_ == States.MOVE_PLAYER_2:
+            self.player_2()
+        else:
+            raise Exception(f'Unknown type: {type_}')
 
 
 if __name__ == '__main__':
---
+++ src/states.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class States(Enum):
+    PREPARE_PLAYER_1 = 1
+    PREPARE_PLAYER_2 = 2
+    MOVE_PLAYER_1 = 3
+    MOVE_PLAYER_2 = 4
