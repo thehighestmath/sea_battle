e543b9deab894c47515cfe2d1c9db7dc72d5fe21
John C. Alastor <42917151+kinekt2000@users.noreply.github.com>
2021-03-30 02:37:15 +0300

    resource relative path setter
    Made a system smarter than me :)

--- src/Environment.py
+++ src/Environment.py
@@ -5,15 +5,40 @@ import sys
 class Root:
     @staticmethod
     def path():
-        return os.path.join(os.path.dirname(__file__), "..")
+        return os.path.dirname(os.path.dirname(__file__))
 
 
 class Resources:
     @staticmethod
-    def path(relative_path):
+    def path():
+        relativePath = str(ResourceDirectory())
         if hasattr(sys, "_MEIPASS"):
-            return os.path.join(sys._MEIPASS, relative_path)
-        return os.path.join(Root.path(), relative_path)
+            return os.path.join(sys._MEIPASS, relativePath)
+        return os.path.join(Root.path(), relativePath)
 
-if __file__ == "__main__":
-    pass
+class ResourceDirectory(object):
+    def __new__(cls, relative_path = None):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(ResourceDirectory, cls).__new__(cls)
+        if not hasattr(cls, 'relativePath') and relative_path == None:
+            cls.relativePath = "res"
+        elif relative_path:
+            cls.relativePath = relative_path
+            
+        return cls.instance
+
+
+    def __repr__(self):
+        return self.relativePath
+
+    
+    def __str__(self):
+        return self.relativePath
+
+
+if __name__ == "__main__":
+    print("path: ", Resources.path())
+    ResourceDirectory("oioi")
+    print("path: ", Resources.path())
+    
+
