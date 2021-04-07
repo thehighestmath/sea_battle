8bd5b3c9eb163b1ee767f24ed57c326af2bdb37c
Akibardin <42891807+Akibardin@users.noreply.github.com>
2021-04-07 22:47:10 +0300

    Update src/main.py
    Co-authored-by: John C. Alastor <42917151+kinekt2000@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ from MainWindow import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    QFontDatabase.addApplicationFont(os.path.join(Environment.Resources.path(), "fonts", "Roboto", "Roboto-Bold.ttf"))
     ex = MainWindow()
     sys.exit(app.exec_())
