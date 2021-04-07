37c209db33d5a8e97f9249e5e65297210c2991ef
akibardin <akibardin@list.ru>
2021-04-07 20:00:53 +0300

    connect signals from menu widget

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,10 +1,9 @@
 from enum import IntEnum
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QStackedWidget, QAction
+from PyQt5.QtCore import pyqtSlot, QEvent
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QStackedWidget, QAction
 
-# for example
-from PyQt5.QtWidgets import QFormLayout, QLineEdit, QHBoxLayout, QRadioButton, QLabel
 from Presenter.GameWindow import GameWindow
+from OffGame.InitWidget import InitWidget
 
 
 class DisplayedWidget(IntEnum):
@@ -17,13 +16,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        #pug
-        self.menu = QWidget()
+        self.menu = InitWidget()
         self.gameWindow = GameWindow()
         self.currentWidget = DisplayedWidget.MENU
 
-        self.stack1UI()
-        # self.stack2UI()
+        self.menu.PvAISignal.connect(self.goToGameWindow)
+        self.menu.PvPSignal.connect(self.goToGameWindow)
 
         self.Stack = QStackedWidget(self)
         self.Stack.addWidget(self.menu)
@@ -34,10 +32,6 @@ class MainWindow(QMainWindow):
         self.menuButton.clicked.connect(self.goToMenu)
         self.menuButton.setVisible(False)
 
-        self.gameButton = QAction()
-        self.gameButton.setText('Начать игру')
-        self.gameButton.triggered.connect(self.goToGameWindow)
-
         self.menuButton = QAction()
         self.menuButton.setText('Вернуться в меню')
         self.menuButton.triggered.connect(self.goToMenu)
@@ -45,7 +39,6 @@ class MainWindow(QMainWindow):
 
         self.toolbar = self.addToolBar('toolbar')
         self.toolbar.addAction(self.menuButton)
-        self.toolbar.addAction(self.gameButton)
         self.toolbar.setMovable(False)
 
         self.setGeometry(0, 0, 800, 600)
@@ -56,29 +49,13 @@ class MainWindow(QMainWindow):
     def goToMenu(self):
         self.currentWidget = DisplayedWidget.MENU
         self.menuButton.setVisible(False)
-        self.gameButton.setVisible(True)
         self.display()
 
     @pyqtSlot()
     def goToGameWindow(self):
         self.currentWidget = DisplayedWidget.GAME
-        self.gameButton.setVisible(False)
         self.menuButton.setVisible(True)
         self.display()
 
     def display(self):
         self.Stack.setCurrentIndex(self.currentWidget)
-
-    # this for example
-    def stack1UI(self):
-        layout = QFormLayout()
-        layout.addRow("PUG", QLineEdit("yea"))
-        self.menu.setLayout(layout)
-
-    def stack2UI(self):
-        layout = QFormLayout()
-        result = QHBoxLayout()
-        result.addWidget(QRadioButton("Win"))
-        result.addWidget(QRadioButton("Lose"))
-        layout.addRow(QLabel("?"), result)
-        self.gameWindow.setLayout(layout)
