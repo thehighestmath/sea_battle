802e406ad4c471c529165893f17e4bbba0459494
John C. Alastor <42917151+kinekt2000@users.noreply.github.com>
2021-04-17 00:39:09 +0300

    enqueue events

--- src/Model/Controller.py
+++ src/Model/Controller.py
@@ -1,20 +1,21 @@
 import logging
+from collections import deque
 from PyQt5.QtCore import QObject
 from PyQt5.QtCore import pyqtSignal
 
 log = logging.getLogger("GameArea")
 
 class Controller(QObject):
-    hit = pyqtSignal(int, int)
+    hit = pyqtSignal(Controller, int, int)
     
     def __init__(self):
         super(Controller, self).__init__()
-        self.__lastHit = None
+        self.__lastHit = deque()
 
     def emitHit(self, x, y):
         log.debug(f"emit hit on ({x}, {y})")
-        self.__lastHit = (x, y)
-        self.hit.emit(x, y)
+        self.__lastHit.append((x, y))
+        self.hit.emit(self, x, y)
 
     def _accept(self):
         raise NotImplementedError()
@@ -23,19 +24,17 @@ class Controller(QObject):
         raise NotImplementedError()
 
     def accept(self):
-        if self.__lastHit is None:
+        if not self.__lastHit:
             raise LookupError("Event already handled")
 
-        x, y = self.__lastHit
-        self.__lastHit = None
+        x, y = self.__lastHit.popleft()
         self._accept(x, y)
 
     def decline(self):
-        if self.__lastHit is None:
+        if not self.__lastHit:
             raise LookupError("Event already handled")
 
-        x, y = self.__lastHit
-        self.__lastHit = None
+        x, y = self.__lastHit.popleft()
         self._decline(x, y)
 
 
