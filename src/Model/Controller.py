f3250d01036fd7cc544b7a5cdbf8986ba7b06c37
John C. Alastor <42917151+kinekt2000@users.noreply.github.com>
2021-04-19 18:31:01 +0300

    display hits

--- src/Model/Controller.py
+++ src/Model/Controller.py
@@ -16,7 +16,6 @@ class Controller(QObject):
         log.debug(f"emit hit on ({x}, {y})")
         self.__lastHit.append((x, y))
         self.hit.emit(self, x, y)
-        print(self.__lastHit)
 
     def _accept(self):
         raise NotImplementedError()
@@ -24,12 +23,12 @@ class Controller(QObject):
     def _decline(self):
         raise NotImplementedError()
 
-    def accept(self):
+    def accept(self, hit_type="miss"):
         if not self.__lastHit:
             raise LookupError("Event already handled")
 
         x, y = self.__lastHit.popleft()
-        self._accept(x, y)
+        self._accept(x, y, hit_type)
 
     def decline(self):
         if not self.__lastHit:
